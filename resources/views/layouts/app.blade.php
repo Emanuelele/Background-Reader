c23067efe51dbf545a17e9c507334dbb8243411c
lele <emanuele.vitt@gmail.com>
2024-02-03 04:31:49 +0100

    Project refactoring

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -169,7 +169,6 @@
         <!-- notify -->
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
         <!-- main js -->
-        <script src="{{ asset('assets/js/dashboard.js') }}"></script>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="{{ asset('assets/js/main.js') }}"></script>
         <!-- others -->
